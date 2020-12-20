24b4d06b7fdfb8d0f35195d70b980f4c4ae2bbcd
Clare Jackson <clare-jackson@lambdastudents.com>
2020-12-20 12:20:14 -0500

    More setup.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
+import { addFeatureReducer } from './addFeatureReducer'
+import { removeFeatureReducer } from './removeFeatureReducer'
 
 
 export const rootReducer = combineReducer({
-  
+    addFeature: addFeatureReducer,
+    removeFeature: removeFeatureReducer
 })
